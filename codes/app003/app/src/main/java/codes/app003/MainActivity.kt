b3e0ace5c176fb92e59b3a81d3fad9a12402aae7
silongboai <silongboai@gmail.com>
2024-03-01 10:00:07 -0500

    $

---
+++ codes/app003/app/src/main/java/codes/app003/MainActivity.kt
@@ -0,0 +1,90 @@
+package codes.app003
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import codes.app003.ui.theme.App003Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App003Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Article1()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Article1() {
+    ArticleCard(
+        headImage = painterResource(id = R.drawable.bg_compose_background),
+        title = stringResource(id = R.string.str1),
+        shortDescription = stringResource(id = R.string.str2),
+        longDescription = stringResource(id = R.string.str3)
+    )
+}
+
+@Composable
+fun ArticleCard(
+    headImage: Painter,
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Image(
+            painter = headImage,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = shortDescription,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = longDescription,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun App003Preview() {
+    App003Theme {
+        Article1()
+    }
+}
