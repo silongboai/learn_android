b78a96b58dfd275f2aa9c27a9631f27c27fd7cd9
silongboai <silongboai@gmail.com>
2024-03-09 16:48:54 -0500

    $

---
+++ codes/app009/app/src/main/java/codes/app009/model/Topic.kt
@@ -0,0 +1,10 @@
+package codes.app009.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int,
+)
